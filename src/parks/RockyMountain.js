baa545fd67f8b88985952e995aa3b4950139f765
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-07-12 17:35:50 +0300

    commit tree e735a4062a37d1a65253864fc2f931d6fdd05b57

--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -4,8 +4,10 @@ function wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
 }
 
+
 function elevation() {
   console.log("9583 ft");
 }
 
-export { trees, wildlife };
+
+export { trees, wildlife, elevation };
